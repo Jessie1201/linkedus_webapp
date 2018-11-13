d75a8376cef483fa011890592e063dc931ae71e5
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2018-11-14 00:04:45 +0100

    location filter menu UI added, not yet connected with db

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from 'material-ui-search-bar';
+// for location filter
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 import './Home.css';
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
 
 class Home extends React.Component{
 
-  constructor() {
-    super();
-    this.state = {
-      isActive: false    
-    }
-  }
+  // for location filter
+  state = {
+    anchorEl: null,
+    selectedIndex: 0,
+  };
+
+  handleClickListItem = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMenuItemClick = (event, index) => {
+    this.setState({ selectedIndex: index, anchorEl: null });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
 
   render() {
+    const { anchorEl } = this.state;
+
     return (
       <div className='phone'>
+
         <div className='phone__header'>
           <Link to="/profile" style={{ color:'white' }}>
-            <span className='fa fa-user-circle'/>
+            <span className='fa fa-user-circle' />
           </Link>
+
           <SearchBar
             onChange={() => console.log('onChange')}
             onRequestSearch={() => console.log('onRequestSearch')}
@@ -32,8 +54,38 @@ class Home extends React.Component{
               borderRadius: '30px',
             }}
           />
-          <span className="fas fa-compass"/>
+
+          <List>
+            <ListItem
+              disableGutters={true}
+              onClick={this.handleClickListItem}
+            >
+              <span className="fas fa-compass"/>
+            </ListItem>
+          </List>
         </div>
+
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 7.5,
+            },
+          }}
+        >
+          {options.map((option, index) => (
+            <MenuItem
+              key={option}
+              selected={index === this.state.selectedIndex}
+              onClick={event => this.handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+
         <div className='phone-content__wrapper'>
           <CardContent color='blue'/>
           <CardContent color='blue' />
@@ -45,7 +97,6 @@ class Home extends React.Component{
     );
   }
 }
-
 
 
 class CardContent extends React.Component{
@@ -91,5 +142,51 @@ class CardContent extends React.Component{
     );
   }
 }
+
+
+// for location filter
+const ITEM_HEIGHT = 48;
+const options = [
+  'Stockholm',
+  'Gothenburg',
+  'Malmö',
+  'Västerås',
+  'Uppsala',
+  'Helsingborg',
+  'Jönköping',
+  'Gävle',
+  'Linköping',
+  'Norrköping',
+  'Örebro',
+  'Växjö',
+  'Halmstad',
+  'Karlstad',
+  'Lund',
+  'Luleå',
+  'Umeå',
+  'Borås',
+  'Karlskrona',
+  'Falun',
+  'Östersund',
+  'Sundsvall',
+  'Södertälje',
+  'Skövde',
+  'Visby',
+  'Uddevalla',
+  'Trollhättan',
+  'Huskvarna',
+  'Ystad',
+  'Piteå',
+  'Falkenberg',
+  'Härnösand',
+  'Nyköping',
+  'Varberg',
+  'Oskarshamn',
+  'Kiruna',
+  'Skanör med Falsterbo',
+  'Kristianstad',
+  'Torshälla',
+];
+
 
 export default Home;
