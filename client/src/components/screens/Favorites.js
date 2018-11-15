5c74af6a90a05d39aea745390cb64776787a0644
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2018-11-15 13:32:33 +0100

    favorites screen layout done, saved cards yet to be matched

--- client/src/components/screens/Favorites.js
+++ client/src/components/screens/Favorites.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import './Favorites.css';
+import SearchBar from 'material-ui-search-bar';
+import CardContent from '../Card.js';
 
 
-class FavoritesScreen extends React.Component {
+class FavoritesScreen extends React.Component{
+
   render() {
+
     return (
-    	<div>
-    		<span>Favorites Screen</span>
+      <div className='phone'>
 
-    	</div>
+        <div className='phone__header'>
+          <SearchBar
+            onChange={() => console.log('onChange')}
+            onRequestSearch={() => console.log('onRequestSearch')}
+            style={{
+              backgroundColor: 'rgba(255,255,255,0.3)',
+              width: '100%',
+              height: '60%',
+              margin: '0 1em',
+              boxShadow: 'none',
+              borderRadius: '30px',
+            }}
+          />
+        </div>
 
+        <div
+        	style={{
+        		top: '55px',
+        		height: '28px',
+        		textAlign: 'center',
+        		fontSize: '1.3em',
+        		fontStyle: 'italic',
+        		fontWeight: 'bold',
+        		color: '#8A94AF',
+        	}}
+        >Saved Contacts</div>
+
+        <div className='phone-content__wrapper'>
+          <CardContent />
+          <CardContent />
+          <CardContent />
+          <CardContent />
+        </div>
+
+      </div>
     );
   }
 }
+
 
 export default FavoritesScreen;
